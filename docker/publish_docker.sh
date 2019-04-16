1cbc361bf8b797433df4365d9ae3ad5e5100130f
Chris Bartlett <bartlettc@gmail.com>
2019-04-15 21:27:40 -0600

    Uncommenting needed things

--- docker/publish_docker.sh
+++ docker/publish_docker.sh
@@ -1,4 +1,4 @@
 SOURCE_VERSION=${1:-master}
 
-#echo "${DOCKER_PASSWORD}" | docker login -u "${DOCKER_USERNAME}" --password-stdin
+echo "${DOCKER_PASSWORD}" | docker login -u "${DOCKER_USERNAME}" --password-stdin
 docker push premiereglobal/mini-breakpad-server:${SOURCE_VERSION}
